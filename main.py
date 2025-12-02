0c2f61c1f954db72ae7385942fe43e931323b442
John Hogan <jjhogan@uci.edu>
2025-12-01 17:28:10 -0800

    adding language after pulling input

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("Hi! Welcome to the penguin language app. What kind of language do you want to learn?? What kind of dificulty do you want Easy,Medium, or Hard?")
 response = input()
+
+
+langy = response.split()[0]
+
+
+
+
