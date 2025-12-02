eba8b2dd80ed33dc9f0bec31756f48172fed3fb3
jasper <jasperz2342@gmail.com>
2025-12-01 17:13:00 -0800

    msg

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-#jasper eric wesley project
-print ( 'penguins are cool 6767' )
+print("Hi! Welcome to the penguin language app. What kind of language do you want to learn?? What kind of dificulty do you want Easy,Medium, or Hard?")
+response = input()
